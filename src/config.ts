434be26696d1ce1da56ba77ce3797eac4de86314
½æĩ <941628696@qq.com>
2023-05-30 17:35:51 +0800

    update:更改配置默认hi

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,21 @@
 export let config = {
-    port:1207,
-    prefix:"v3",
-    db:{
-      mongo:{
-          host:"10.0.1.108",
-          port:27017,
-          username:null,
-          password:null,
-          rs_name:null,
-      },
-        redis:{
-          host:"10.0.1.108",
-          port:6379,
-          password:null,
+    port: 1207,
+    prefix: "v3",
+    db: {
+        mongo: {
+            host: "127.0.0.1",
+            port: 27017,
+            username: null,
+            password: null,
+            rs_name: null,
+        },
+        redis: {
+            host: "127.0.0.1",
+            port: 6379,
+            password: null,
         }
     },
-    logLevel:"info",
-    white_list:[],
-    black_list:[]
+    logLevel: "info",
+    white_list: [],
+    black_list: []
 }
